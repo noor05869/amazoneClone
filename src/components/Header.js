d4705acc7c0964c6992f86d826aa2ec85c432d65
noor05869 <noor05869@gmail.com>
2022-01-31 19:52:22 +0500

    first

---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+function Header() {
+  return (
+    <div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className=" mt-2 flex items-center sm:flex-grow-0 ">
+          <Image
+            objectFit="contain"
+            src="https://links.papareact.com/f90"
+            height={40}
+            width={150}
+            className="cursor-pointer"
+          />
+        </div>
+        <div className=" hidden sm:flex items-center h-10 rounded-md cursor-pointer flex-grow bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* <right> */}
+        <div className="flex items-center text-xs space-x-6 mx-6 whitespace-nowrap text-white ">
+          <div className=" link">
+            <p className="  text-white">yaooo</p>
+            <p className="font-extrabold md:text-sm link">
+              acount list &othher{" "}
+            </p>
+          </div>
+          <div className=" link">
+            <p>returns </p>
+            <p className="font-extrabold md:text-sm ">orders </p>
+          </div>
+          <div className=" relative flex items-center link">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full   text-black font-bold        ">
+              {" "}
+              0
+            </span>
+            <ShoppingCartIcon className=" hidden md:inline h-10" />
+            <p className="font-extrabold md:text-sm "> baskit</p>
+          </div>
+        </div>
+      </div>
+      <div className=" flex space-x-3 p-4 pl-6  items-center bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center ">
+          <MenuIcon className="h-6 mr-1" />
+          all
+        </p>
+        <p className="link "> prime vedio</p>
+        <p className="link "> amazone buisness </p>
+
+        <p className="link "> todays deal</p>
+
+        <p className="link hidden lg:inline-flex "> electronics</p>
+        <p className="link hidden lg:inline-flex "> food and grocery </p>
+
+        <p className="link hidden lg:inline-flex ">prime</p>
+
+        <p className="link hidden lg:inline-flex "> buy agian</p>
+
+        <p className="link hidden lg:inline-flex "> shopper toolkit</p>
+
+        <p className="link hidden lg:inline-flex ">
+          {" "}
+          Health and professional care
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      {/* <header></header> */}
+
+      <h1>hello</h1>
+      <Header />
+    </div>
+  );
+}
