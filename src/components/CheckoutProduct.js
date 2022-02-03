f8633f3ec7e1a836860e5fa51bcaf6146a9cf7ea
noor05869 <noor05869@gmail.com>
2022-02-03 19:20:39 +0500

    checkout section and logic

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import { removeFromBasket } from "../slices/basketSlice";
+import { useSession } from "next-auth/react";
 function CheckoutProduct({ items }) {
   console.log("items", items);
   const router = useRouter();
+  const { data: session } = useSession();
   const dispatch = useDispatch();
 
   function removeFromeBasket(id) {
@@ -121,55 +123,67 @@ function CheckoutProduct({ items }) {
                     </div>
                   ))}
               </div>
-              <div className=" flex sm:w-2 xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full">
-                <div className="flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
-                  <div>
-                    <p className="text-4xl font-black leading-9 text-gray-800">
-                      Summary
-                    </p>
-                    <div className="flex items-center justify-between pt-16">
-                      <p className="text-base leading-none text-gray-800">
-                        Subtotal
+              {items.length > 0 ? (
+                <div className=" flex sm:w-2 xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full">
+                  <div className="flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
+                    <div>
+                      <p className="text-4xl font-black leading-9 text-gray-800">
+                        Summary
                       </p>
-                      <p className="text-base leading-none text-gray-800">
-                        $9,000
-                      </p>
+                      <div className="flex items-center justify-between pt-16">
+                        <p className="text-base leading-none text-gray-800">
+                          Subtotal
+                        </p>
+                        <p className="text-base leading-none text-gray-800">
+                          $9,000
+                        </p>
+                      </div>
+                      <div className="flex items-center justify-between pt-5">
+                        <p className="text-base leading-none text-gray-800">
+                          Shipping
+                        </p>
+                        <p className="text-base leading-none text-gray-800">
+                          $30
+                        </p>
+                      </div>
+                      <div className="flex items-center justify-between pt-5">
+                        <p className="text-base leading-none text-gray-800">
+                          Tax
+                        </p>
+                        <p className="text-base leading-none text-gray-800">
+                          $35
+                        </p>
+                      </div>
                     </div>
-                    <div className="flex items-center justify-between pt-5">
-                      <p className="text-base leading-none text-gray-800">
-                        Shipping
-                      </p>
-                      <p className="text-base leading-none text-gray-800">
-                        $30
-                      </p>
+                    <div>
+                      <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
+                        <p className="text-2xl leading-normal text-gray-800">
+                          Total
+                        </p>
+                        <p className="text-2xl font-bold leading-normal text-right text-gray-800">
+                          $10,240
+                        </p>
+                      </div>
+                      <div className="mt-6">
+                        <button
+                          onClick={() => setShow(!show)}
+                          className={`text-base leading-none w-full py-5 ${
+                            !session
+                              ? "bg-yellow-400 hover:bg-yellow-500 text-black"
+                              : "bg-gray-800 hover:bg-gray-900"
+                          }  border-gray-800  rounded  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white`}
+                        >
+                          {!session
+                            ? "sign in to checkout"
+                            : "proceed to checkout"}
+                        </button>
+                      </div>
                     </div>
-                    <div className="flex items-center justify-between pt-5">
-                      <p className="text-base leading-none text-gray-800">
-                        Tax
-                      </p>
-                      <p className="text-base leading-none text-gray-800">
-                        $35
-                      </p>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
-                      <p className="text-2xl leading-normal text-gray-800">
-                        Total
-                      </p>
-                      <p className="text-2xl font-bold leading-normal text-right text-gray-800">
-                        $10,240
-                      </p>
-                    </div>
-                    <button
-                      onClick={() => setShow(!show)}
-                      className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white"
-                    >
-                      Checkout
-                    </button>
                   </div>
                 </div>
-              </div>
+              ) : (
+                <p>noitems</p>
+              )}
             </div>
           </div>
         </div>
