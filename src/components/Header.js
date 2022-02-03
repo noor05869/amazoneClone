7adcd08b1b545be0842d0965bc40bc41ff567dd0
noor05869 <noor05869@gmail.com>
2022-02-03 16:34:24 +0500

    checkout modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,7 +53,7 @@ function Header() {
             onClick={() => router.push("/Checkout")}
             className=" relative flex items-center link"
           >
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full   text-black font-bold        ">
+            <span className="absolute top-0 right-10  md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full   text-black font-bold        ">
               {" "}
               {items.length}
             </span>
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
 function Checkout() {
+  const items = useSelector(selectItems);
+  console.log("first", items.image);
   return (
-    <div className="bg-gray-100">
-      <Header />
-      <div className="  lg:flex  max-w-screen-2xl  mx-auto">
-        <div className="flex-grow m-5 shadow-sm">
-          <Image
-            src=" https://links.papareact.com/ikj"
-            width={1020}
-            height={250}
-            objectFit="contain"
-          />
-          <div className="flex flex-col p-5 space-y-10 bg-white">
-            <h1 className="text-3xl border-b pb-4">shopiign baskit</h1>
-          </div>
-        </div>
-      </div>
+    // <div className="bg-gray-100">
+    //   <Header />
+    //   <div className="  lg:flex  max-w-screen-2xl  mx-auto">
+    //     <div className="flex-grow m-5 shadow-sm">
+    //       <Image
+    //         src=" https://links.papareact.com/ikj"
+    //         width={1020}
+    //         height={250}
+    //         objectFit="contain"
+    //       />
+    <div className="flex flex-col p-5 space-y-10 bg-white">
+      <CheckoutProduct items={items} />
     </div>
+    //     </div>
+    //   </div>
   );
 }
 
